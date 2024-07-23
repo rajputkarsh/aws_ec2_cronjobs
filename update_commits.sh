472ac06689d6dab4307c0821f344bf716e73974f
Utkarsh <r.utkarsh.0010@gmail.com>
2024-07-24 05:26:05 +0530

    Update update_commits.sh

--- update_commits.sh
+++ update_commits.sh
@@ -10,4 +10,4 @@ timestamp=$(date -Iseconds)
 filename="outputs/api_response_$timestamp.txt"
 
 # Make the API call and save the result to the file in the script's directory
-curl -X GET "https://utkarsh.app/api/update-commits" -o "$script_dir/$filename"
+curl --location "https://utkarsh.app/api/update-commits" -o "$script_dir/$filename"
